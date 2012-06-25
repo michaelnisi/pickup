b9e2ed8a6db0543225da966d1d2e898f4e78471b
Michael Nisi <michael.nisi@gmail.com>
2012-06-25 18:51:59 +0200

    Parse podcast information

--- example/stdout.js
+++ example/stdout.js
@@ -1,7 +1,5 @@
 var path = './test/iTunes.xml'
 ,   fstream = require('fs').createReadStream(path)
-,   radio = require('../lib/radio.js')()
+,   pickup = require('../lib/radio.js')()
 
-fstream.pipe(radio).pipe(process.stdout)
-
-
+fstream.pipe(pickup).pipe(process.stdout)
