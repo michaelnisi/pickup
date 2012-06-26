144b0391de4ee0758e6378cd95f196395eac2b1d
Michael Nisi <michael.nisi@gmail.com>
2012-06-26 20:42:21 +0200

    Write test

--- test/iTunes.js
+++ test/iTunes.js
@@ -19,6 +19,12 @@ test('iTunes', function (t) {
 function getExpected () {
   return [
     { key: 'title', value: 'All About Everything' }
+  , { key: 'link', value: 'http://www.example.com/podcasts/everything/index.html' }
+  , { key: 'language', value: 'en-us' }
+  , { key: 'subtitle', value: 'A show about everything' }
+  , { key: 'author', value: 'John Doe' }
+  , { key: 'summary', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
+  , { key: 'description', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
   , { key: 'response', value: 200 }
   ]
 }
