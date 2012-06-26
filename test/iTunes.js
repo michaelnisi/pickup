8cef2ef500bd676e432ade1d4ad79b8f3ccd108c
Michael Nisi <michael.nisi@gmail.com>
2012-06-26 21:13:51 +0200

    Write test

--- test/iTunes.js
+++ test/iTunes.js
@@ -4,9 +4,18 @@ var path = './test/iTunes.xml'
 ,   test = require('tap').test
 ,   es = require('event-stream')
 
+var expected = [
+  { key: 'title', value: 'All About Everything' }
+, { key: 'link', value: 'http://www.example.com/podcasts/everything/index.html' }
+, { key: 'language', value: 'en-us' }
+, { key: 'subtitle', value: 'A show about everything' }
+, { key: 'author', value: 'John Doe' }
+, { key: 'summary', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
+, { key: 'description', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
+, { key: 'response', value: 200 }
+]
+
 test('iTunes', function (t) {
-  var expected = getExpected()
-  
   es.connect(
     fstream.pipe(pickup),
     es.writeArray(function (err, lines) {
@@ -16,15 +25,4 @@ test('iTunes', function (t) {
   )   
 })
 
-function getExpected () {
-  return [
-    { key: 'title', value: 'All About Everything' }
-  , { key: 'link', value: 'http://www.example.com/podcasts/everything/index.html' }
-  , { key: 'language', value: 'en-us' }
-  , { key: 'subtitle', value: 'A show about everything' }
-  , { key: 'author', value: 'John Doe' }
-  , { key: 'summary', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
-  , { key: 'description', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
-  , { key: 'response', value: 200 }
-  ]
-}
+
