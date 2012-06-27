cf8ae93368c72cb4217553d2b28ecbae29448927
Michael Nisi <michael.nisi@gmail.com>
2012-06-27 20:06:12 +0200

    Hack it to succeed

--- example/proxy.js
+++ example/proxy.js
@@ -4,11 +4,13 @@ var http = require('http')
 ,   pickup = require('../lib/pickup.js')()
   
 http.createServer(function (req, resp) {
-  var uri = url.parse(req.url, true).query.uri
-  if (!uri) {
-    resp.writeHead(400)
-    resp.end()
-    return
-  }
-  request(uri).pipe(pickup).pipe(resp)  
+  req.on('end', function () {
+    var uri = url.parse(req.url, true).query.uri
+    if (!uri) {
+      resp.writeHead(400)
+      resp.end()
+      return
+    }  
+    request(uri).pipe(pickup).pipe(resp)  
+  })
 }).listen(8080, '127.0.0.1')
