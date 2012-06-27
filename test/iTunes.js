4f5c7ac996149e91890fdfe888c5cc278bfd297d
Michael Nisi <michael.nisi@gmail.com>
2012-06-27 12:34:27 +0200

    Resolve ignored tag issue

--- test/iTunes.js
+++ test/iTunes.js
@@ -12,6 +12,26 @@ var expected = [
 , { key: 'author', value: 'John Doe' }
 , { key: 'summary', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
 , { key: 'description', value: 'All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our Podcast in the iTunes Store' }
+, { key: 'image', value: { href: 'http://example.com/podcasts/everything/AllAboutEverything.jpg' } }
+, { key: 'item', value: { 
+      title: 'Shake Shake Shake Your Spices' 
+    , author: 'John Doe' 
+    , subtitle: 'A short primer on table spices'
+    , summary: 'This week we talk about salt and pepper shakers, comparing and contrasting pour rates, construction materials, and overall aesthetics. Come and join the party!' 
+    , image: { 
+      href: 'http://example.com/podcasts/everything/AllAboutEverything/Episode1.jpg'
+    }
+    , enclosure: {
+      url: 'http://example.com/podcasts/everything/AllAboutEverythingEpisode3.m4a'
+    , length: '8727310'
+    , type: 'audio/x-m4a'
+    }   
+    , guid: 'http://example.com/podcasts/archive/aae20050615.m4a'
+    , pubDate: 'Wed, 15 Jun 2005 19:00:00 GMT'
+    , duration: '7:04'
+    , keywords: 'salt, pepper, shaker, exciting'
+  } 
+}
 , { key: 'response', value: 200 }
 ]
 
