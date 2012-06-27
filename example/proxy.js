146e30be8f51d1df0c10708b248693c9e5a112d4
Michael Nisi <michael.nisi@gmail.com>
2012-06-27 15:47:37 +0200

    Add proxy example

--- example/proxy.js
+++ example/proxy.js
@@ -1,12 +1,10 @@
-// http://127.0.0.1:8080/?http://feeds.feedburner.com/back2work
-
 var http = require('http')
 ,   url = require('url')
 ,   request = require('request')
 ,   pickup = require('../lib/pickup.js')()
   
 http.createServer(function (req, resp) {
-  var uri = url.parse(req.url, true).query.url
+  var uri = url.parse(req.url, true).query.uri
   if (!uri) {
     resp.writeHead(400)
     resp.end()
