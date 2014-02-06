1ec3be9067926393d7f6e2e1636bd259963e267d
Michael Nisi <michael.nisi@gmail.com>
2014-02-06 17:28:55 +0100

    Fix feed title bug

---
+++ test/feed_link.js
@@ -0,0 +1,32 @@
+
+var test = require('tap').test
+  , transform = require('./transform')
+  , pickup = require('../')
+  , stread = require('stread')
+  , Writable = require('stream').Writable
+
+test('rss', function (t) {
+  var found = ''
+    , writer = new Writable()
+
+  stread(xml())
+    .pipe(pickup())
+    .pipe(writer)
+    .on('finish', function () {
+      t.is(found, wanted())
+      t.end()
+    })
+
+  writer._write = function (chunk, enc, cb) {
+    found += chunk
+    cb()
+  }
+})
+
+function xml () {
+  return '<rss><channel><atom:link rel="via" href="http://feeds.muleradio.net/thetalkshow"/><atom:link href="http://feeds.muleradio.net/thetalkshow" rel="self" type="application/rss+xml"/><title>The Talk Show With John Gruber</title><link>http://muleradio.net/thetalkshow</link></channel></rss>'
+}
+
+function wanted () {
+  return '{"feed":{"link":"http://feeds.muleradio.net/thetalkshow","title":"The Talk Show With John Gruber"}}'
+}
