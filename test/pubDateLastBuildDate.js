1f220e988d253331fe24db88fbabdf49fdf58d23
Chris Roberts <1645367+naxxfish@users.noreply.github.com>
2018-12-28 10:06:13 +0000

    Add a test for different orders for pubDate and lastBuildDate

--- test/pubDateLastBuildDate.js
+++ test/pubDateLastBuildDate.js
@@ -4,7 +4,26 @@
 
 const parse = require('./lib/parse')
 const test = require('tap').test
-
+test('updated populated by lastBuildDate', function (t) {
+    const xml = [
+      '<rss><channel>',
+      '<lastBuildDate>2018-12-27T23:29:49+0000</lastBuildDate>',
+      '</channel></rss>'
+    ].join()
+    const wanted = [
+      ['feed', {
+        updated: '2018-12-27T23:29:49+0000'
+      }],
+      ['readable'],
+      ['finish'],
+      ['end']
+    ]
+    parse({ t: t, xml: xml, wanted: wanted }, (er) => {
+      t.ok(!er)
+      t.end()
+    })
+  })
+  
 test('pubDate overrides lastBuildDate', function (t) {
   const xml = [
     '<rss><channel>',
@@ -25,16 +44,16 @@ test('pubDate overrides lastBuildDate', function (t) {
     t.end()
   })
 })
-
-test('updated populated by lastBuildDate', function (t) {
+test('pubDate overrides lastBuildDate in any order', function (t) {
   const xml = [
     '<rss><channel>',
+    '<pubDate>2018-12-27T23:29:50+0000</pubDate>',
     '<lastBuildDate>2018-12-27T23:29:49+0000</lastBuildDate>',
     '</channel></rss>'
   ].join()
   const wanted = [
     ['feed', {
-      updated: '2018-12-27T23:29:49+0000'
+      updated: '2018-12-27T23:29:50+0000'
     }],
     ['readable'],
     ['finish'],
@@ -45,3 +64,4 @@ test('updated populated by lastBuildDate', function (t) {
     t.end()
   })
 })
+
