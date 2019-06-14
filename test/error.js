99d435c56517eb4b2cba74181381cf798ac21814
Michael Nisi <michael.nisi@gmail.com>
2019-06-14 07:09:49 +0200

    Audit and housekeeping

--- test/error.js
+++ test/error.js
@@ -9,7 +9,7 @@ test('whitespace', (t) => {
   const xml = 'wtf'
 
   const wanted = [
-    ['error', new Error('Non-whitespace before first tag.')],
+    ['error', new Error('undefined:1:1: text data outside of root node.')],
     ['error', new Error('undefined:1:3: document must contain a root element.')],
     ['finish'],
     ['end']
